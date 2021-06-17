8cf7f3a410b6c4fb19ea0b33c12e2478edf5d06a
daison324 <daison.ueki.324@gmail.com>
2021-06-17 17:03:05 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    with_options presence: true do
+  with_options presence: true do
       validates :nickname
       validates :birthday
+  end
 
-      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
-        validates :first_name
-        validates :last_name
-      end
-
-      with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width characters' } do
-        validates :first_name_kana
-        validates :last_name_kana
-      end
-    end
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
-     has_many :groups
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
 end
+with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+end
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+has_many :groups
+has_many :comments
+
+end 
 
